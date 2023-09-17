54a39173febb3cb7da5f83d13db4f28022ab12cc
jakexcode <jakexcode@gmail.com>
2023-09-16 22:25:13 -0400

    Updates

---
+++ precache-manifest.e3cdb20c6b4e6b11eb9dc8217073bd3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba0f197dbecc430f011c20a51f7bf79",
+    "url": "/Pomodoro-Project/index.html"
+  },
+  {
+    "revision": "42432e56bf62787e8ab1",
+    "url": "/Pomodoro-Project/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "7a2eb74e9d1ea35325a7",
+    "url": "/Pomodoro-Project/static/js/2.820e90ce.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pomodoro-Project/static/js/2.820e90ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42432e56bf62787e8ab1",
+    "url": "/Pomodoro-Project/static/js/main.4442a527.chunk.js"
+  },
+  {
+    "revision": "e6003f0ba9ca82463d1c",
+    "url": "/Pomodoro-Project/static/js/runtime-main.c23b412e.js"
+  }
+]);
